ce27edae45642b952ab315ec10032cd25234b2ad
Ali Khansari <webconsultant247@gmail.com>
2024-12-15 18:43:04 -0600

    Dynamic Behavior for NextJS

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -1,3 +1,4 @@
+export const dynamic = "force-dynamic";
 import Header from "@/components/Header";
 import MobileNavigation from "@/components/MobileNavigation";
 import Sidebar from "@/components/Sidebar";
